cdd0beb725b488fe4223e8c4ad0e7073de344b42
EnzoSilva14 <enzodrbs@al.insper.ed.br>
2025-03-10 10:37:42 -0300

    exe1

--- exe1/main.c
+++ exe1/main.c
@@ -6,15 +6,22 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile int timer_state = 0;
+
+bool timer_callback(repeating_timer_t *rt) {
+    timer_state = 1;
+    return true; 
+}
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
-        flag_f_r = 1;
+        flag_f_r = !flag_f_r;
     } else if (events == 0x8) { // rise edge
     }
 }
 
 int main() {
+
     stdio_init_all();
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
@@ -26,10 +33,19 @@ int main() {
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                        &btn_callback);
 
-    while (true) {
+    repeating_timer_t timer;
 
-        if (flag_f_r) {
-            flag_f_r = 0;
+    while (true) {
+        if (flag_f_r && !timer_state) {
+            add_repeating_timer_ms(500, timer_callback, NULL, &timer);
+        }
+        if (timer_state){
+            gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R) );
+            timer_state = 0;
+        }
+        if (!flag_f_r) {
+            cancel_repeating_timer(&timer);
+            gpio_put(LED_PIN_R, 0);
         }
     }
 }

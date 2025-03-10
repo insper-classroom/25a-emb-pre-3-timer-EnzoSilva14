8b565440b8377a8d07309b4fa408fa55fc105b7b
EnzoSilva14 <enzodrbs@al.insper.ed.br>
2025-03-10 11:35:51 -0300

    exe2

--- exe2/main.c
+++ exe2/main.c
@@ -8,15 +8,25 @@ const int BTN_PIN_G = 26;
 const int LED_PIN_R = 4;
 const int LED_PIN_G = 6;
 
-volatile int flag_r = 0;
-volatile int flag_g = 0;
+volatile int flag_f_r = 0;
+volatile int flag_f_g = 0;
 
+bool timer_callback_r(repeating_timer_t *rt) {
+    bool state_r = gpio_get(LED_PIN_R);
+    gpio_put(LED_PIN_R , !state_r);
+    return true; 
+}
+bool timer_callback_g(repeating_timer_t *rt) {
+    bool state_g = gpio_get(LED_PIN_G);
+    gpio_put(LED_PIN_G , !state_g);
+    return true; 
+}
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) {
         if (gpio == BTN_PIN_R)
-            flag_r = 1;
+            flag_f_r = 1;
         else if (gpio == BTN_PIN_G)
-            flag_g = 1;
+            flag_f_g = 1;
     }
 }
 
@@ -39,14 +49,37 @@ int main() {
                                        &btn_callback);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
+    repeating_timer_t timer_red;
+    bool timer_r = false;
+    repeating_timer_t timer_green;
+    bool timer_g = false;
+
     while (true) {
-
-        if (flag_r) {
-            flag_r = 0;
+        // Led Vermelho
+        if (flag_f_r) {
+            flag_f_r = 0;
+            if (timer_r){
+                gpio_put(LED_PIN_R , 0);
+                cancel_repeating_timer(&timer_red);
+                timer_r = false;
+            }
+            else{
+                add_repeating_timer_ms(500 , timer_callback_r , NULL , &timer_red);
+                timer_r = true;
+            }
         }
-
-        if (flag_g) {
-            flag_g = 0;
+        // Led Verde
+        if (flag_f_g) {
+            flag_f_g = 0;
+            if (timer_g){
+                gpio_put(LED_PIN_G , 0);
+                cancel_repeating_timer(&timer_green);
+                timer_g = false;
+            }
+            else{
+                add_repeating_timer_ms(250 , timer_callback_g , NULL , &timer_green);
+                timer_g = true;
+            }
         }
     }
 }
